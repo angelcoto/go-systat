228b520ce3c3eaa1b24388757611754c673d0f82
Ángel Coto <codiasw@protonmail.com>
2023-12-30 21:15:52 -0600

    Incluye opción para imprimir la versión del programa

--- main.go
+++ main.go
@@ -2,12 +2,22 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"os"
 )
+
+const version = "v1.0.0"
 
 func main() {
 
 	optPtr := flag.Bool("p", false, "Lista cada 60 segundos los procesos corriendo en el sistema")
+	verPtr := flag.Bool("v", false, "Imprime la versión")
 	flag.Parse()
+
+	if *verPtr {
+		fmt.Printf("%s\n", version)
+		os.Exit(0)
+	}
 
 	if *optPtr {
 		processesStat()
